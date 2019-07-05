1f917408e3df24659e1459361b26d3ec1d94e4a4
Afnizar Nur Ghifari <afnizarhilmi@gmail.com>
2019-07-05 22:33:36 +0700

    Add functionality

--- src/script.js
+++ src/script.js
@@ -6,15 +6,6 @@ var panelHeight = 242;
 var panelWidth = 343;
 var panelGutter = 16 * 2;
 var container = panelWidth - panelGutter;
-var characters = 0;
-var words = 0;
-var paragraphs = 0;
-
-function getLayers() {
-	const document = sketch.getSelectedDocument()
-	const selectedLayers = document.selectedLayers
-	return selectedLayers
-}
 
 function createFloatingPanel(title, frame) {
 	var panel = NSPanel.alloc().init();
@@ -73,6 +64,7 @@ function createTextDataLabel(string, frame) {
 
 	return field;
 }
+
 function createTextValueLabel(string, frame) {
 	var field = NSTextField.alloc().initWithFrame(frame);
 
@@ -85,27 +77,44 @@ function createTextValueLabel(string, frame) {
 	return field;
 }
 
+function createMainView(characters, words, paragraphs) {
+	var panel = createFloatingPanel(pluginName, NSMakeRect(0, 0, panelWidth, panelHeight));
+	var panelContent = createView(NSMakeRect(0, 0, panelWidth, panelHeight - panelHeader));
+
+	var box = createBox(NSMakeRect(16, 4, container, 145));
+	var labelTotalChar = createTextDataLabel("Total Character", NSMakeRect(35, 35, 120, 15));
+	var valueTotalChar = createTextValueLabel(characters, NSMakeRect(35, 55, 120, 15));
+	var labelWords = createTextDataLabel("Words", NSMakeRect(35, 85, 120, 15));
+	var valueWords = createTextValueLabel(words, NSMakeRect(35, 105, 120, 15));
+	var labelParagraph = createTextDataLabel("Paragraph", NSMakeRect(160, 85, 120, 15));
+	var valueParagraph = createTextValueLabel(paragraphs, NSMakeRect(160, 105, 120, 15));
+
+	var copyLabel = createTextLabel('Easily copy values and sent to your writer', NSMakeRect(16, 163, 250, 20));
+	var copyButton = createButton('Copy', NSMakeRect(260, 162, 70, 23));
+
+	[box, labelTotalChar, valueTotalChar, labelWords, valueWords, labelParagraph, valueParagraph, copyLabel, copyButton].forEach(i => panelContent.addSubview(i));
+
+	panel.contentView().addSubview(panelContent);
+}
+
 export default function () {
-	const selectedLayers = getLayers()
-	if (selectedLayers.length === 0) {
+	var text = context.selection.firstObject();
+	var characters = 0;
+	var words = 0;
+	var paragraphs = 0;
+
+	if (!text || text.class() != "MSTextLayer" || context.selection.length > 1) {
 		sketch.UI.message("⚠️ Please select a text layer to count the character.")
+		return;
 	} else {
-		var panel = createFloatingPanel(pluginName, NSMakeRect(0, 0, panelWidth, panelHeight));
-		var panelContent = createView(NSMakeRect(0, 0, panelWidth, panelHeight - panelHeader));
-		
-		var box = createBox(NSMakeRect(16, 4, container, 145));
-		var labelTotalChar = createTextDataLabel("Total Character", NSMakeRect(35,35,120,15)); 
-		var valueTotalChar = createTextValueLabel(characters, NSMakeRect(35,55,120,15)); 
-		var labelWords= createTextDataLabel("Words", NSMakeRect(35,85,120,15)); 
-		var valueWords = createTextValueLabel(words, NSMakeRect(35,105,120,15)); 
-		var labelParagraph = createTextDataLabel("Paragraph", NSMakeRect(160,85,120,15)); 
-		var valueParagraph = createTextValueLabel(paragraphs, NSMakeRect(160,105,120,15)); 
+		var textAsLayer = text.stringValue();
+		var layerToString = String(textAsLayer);
 
-		var copyLabel = createTextLabel('Easily copy values and sent to your writer', NSMakeRect(16, 163, 250, 20));
-		var copyButton = createButton('Copy', NSMakeRect(260, 162, 70, 23));
-
-		[box, labelTotalChar, valueTotalChar, labelWords, valueWords, labelParagraph, valueParagraph, copyLabel, copyButton].forEach(i => panelContent.addSubview(i));
-
-		panel.contentView().addSubview(panelContent);
+		characters = layerToString.length;
+		words = layerToString.split(/\s+\b/).length;
+		paragraphs = layerToString.replace(/\n$/gm, '').split(/\n/).length;
 	}
+
+	createMainView(characters, words, paragraphs);
+
 }
