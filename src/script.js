03e16852b96880750bcaa1127c23c959ad09f1c6
Afnizar Nur Ghifari <afnizarhilmi@gmail.com>
2019-07-05 20:11:59 +0700

    Finish up building UI

--- src/script.js
+++ src/script.js
@@ -2,23 +2,24 @@ import sketch from 'sketch'
 
 var pluginName = "Character Count"
 var panelHeader = 44;
-var panelFooter = 38;
 var panelHeight = 242;
 var panelWidth = 343;
-var panelGutter = 16*2;
+var panelGutter = 16 * 2;
 var container = panelWidth - panelGutter;
-var panelItems = [];
+var characters = 0;
+var words = 0;
+var paragraphs = 0;
 
-function getLayers(){
-  const document = sketch.getSelectedDocument()
-  const selectedLayers = document.selectedLayers
-  return selectedLayers
+function getLayers() {
+	const document = sketch.getSelectedDocument()
+	const selectedLayers = document.selectedLayers
+	return selectedLayers
 }
 
-function createFloatingPanel(title,frame) {
+function createFloatingPanel(title, frame) {
 	var panel = NSPanel.alloc().init();
 	panel.setTitle(title);
-	panel.setFrame_display(frame,true);
+	panel.setFrame_display(frame, true);
 	panel.setStyleMask(NSTexturedBackgroundWindowMask | NSTitledWindowMask | NSClosableWindowMask | NSFullSizeContentViewWindowMask);
 	panel.setBackgroundColor(NSColor.windowBackgroundColor());
 	panel.setLevel(NSFloatingWindowLevel);
@@ -29,35 +30,30 @@ function createFloatingPanel(title,frame) {
 
 function createView(frame) {
 	var view = NSView.alloc().initWithFrame(frame);
-
 	view.setFlipped(1);
-
 	return view;
 }
 
 function createBox(frame) {
 	var box = NSBox.alloc().initWithFrame(frame);
 	box.setTitle("");
-
 	return box;
 }
 
-function createButton(label,frame) {
+function createButton(label, frame) {
 	var button = NSButton.alloc().initWithFrame(frame);
-
 	button.setTitle(label);
 	button.setBezelStyle(NSRoundedBezelStyle);
 	button.setAction('callAction:');
-
 	return button;
 }
 
-function createTextLabel(string,frame) {
+function createTextLabel(string, frame) {
 	var field = NSTextField.alloc().initWithFrame(frame);
 
 	field.setStringValue(string);
 	field.setFont(NSFont.systemFontOfSize(12));
-	field.setTextColor(NSColor.colorWithCalibratedRed_green_blue_alpha(0,0,0,0.7));
+	field.setTextColor(NSColor.colorWithCalibratedRed_green_blue_alpha(0, 0, 0, 0.7));
 	field.setBezeled(0);
 	field.setBackgroundColor(NSColor.windowBackgroundColor());
 	field.setEditable(0);
@@ -65,21 +61,51 @@ function createTextLabel(string,frame) {
 	return field;
 }
 
-export default function() {
-  const selectedLayers = getLayers()
-  if (selectedLayers.length === 0) {
-    sketch.UI.message("⚠️ Please select a text layer to count the character.")
-  } else {
-	
-	var panel = createFloatingPanel(pluginName,NSMakeRect(0,0,panelWidth,panelHeight));
-	var panelContent = createView(NSMakeRect(0,0,panelWidth,panelHeight - panelHeader));
-	
-	var box = createBox(NSMakeRect(16,4,container,145));
-	var copyLabel = createTextLabel('Easily copy values and sent to your writer', NSMakeRect(16,163,250,20));
-	var copyButton = createButton('Copy',NSMakeRect(260,162,70,23));
-	
-	[box, copyLabel, copyButton].forEach(i => panelContent.addSubview(i));
+function createTextDataLabel(string, frame) {
+	var field = NSTextField.alloc().initWithFrame(frame);
 
-	panel.contentView().addSubview(panelContent);
-  }
+	field.setStringValue(string);
+	field.setFont(NSFont.systemFontOfSize(13));
+	field.setTextColor(NSColor.colorWithCalibratedRed_green_blue_alpha(0, 0, 0, 0.3));
+	field.setBezeled(0);
+	field.setBackgroundColor(NSColor.colorWithCalibratedRed_green_blue_alpha(0, 0, 0, 0));
+	field.setEditable(0);
+
+	return field;
+}
+function createTextValueLabel(string, frame) {
+	var field = NSTextField.alloc().initWithFrame(frame);
+
+	field.setStringValue(string);
+	field.setFont(NSFont.systemFontOfSize(13));
+	field.setBezeled(0);
+	field.setBackgroundColor(NSColor.colorWithCalibratedRed_green_blue_alpha(0, 0, 0, 0));
+	field.setEditable(0);
+
+	return field;
+}
+
+export default function () {
+	const selectedLayers = getLayers()
+	if (selectedLayers.length === 0) {
+		sketch.UI.message("⚠️ Please select a text layer to count the character.")
+	} else {
+		var panel = createFloatingPanel(pluginName, NSMakeRect(0, 0, panelWidth, panelHeight));
+		var panelContent = createView(NSMakeRect(0, 0, panelWidth, panelHeight - panelHeader));
+		
+		var box = createBox(NSMakeRect(16, 4, container, 145));
+		var labelTotalChar = createTextDataLabel("Total Character", NSMakeRect(35,35,120,15)); 
+		var valueTotalChar = createTextValueLabel(characters, NSMakeRect(35,55,120,15)); 
+		var labelWords= createTextDataLabel("Words", NSMakeRect(35,85,120,15)); 
+		var valueWords = createTextValueLabel(words, NSMakeRect(35,105,120,15)); 
+		var labelParagraph = createTextDataLabel("Paragraph", NSMakeRect(160,85,120,15)); 
+		var valueParagraph = createTextValueLabel(paragraphs, NSMakeRect(160,105,120,15)); 
+
+		var copyLabel = createTextLabel('Easily copy values and sent to your writer', NSMakeRect(16, 163, 250, 20));
+		var copyButton = createButton('Copy', NSMakeRect(260, 162, 70, 23));
+
+		[box, labelTotalChar, valueTotalChar, labelWords, valueWords, labelParagraph, valueParagraph, copyLabel, copyButton].forEach(i => panelContent.addSubview(i));
+
+		panel.contentView().addSubview(panelContent);
+	}
 }
